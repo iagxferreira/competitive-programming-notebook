dc0c4d84d581269b3d78a1b21f0d7b70e147b78b
Iago Ferreira <iago-ferreira@outlook.com>
2026-08-29 03:08:15 -0300

    feat: solving subsets with backtracking

--- leetcode/0078-subsets/Solution.java
+++ leetcode/0078-subsets/Solution.java
@@ -1,8 +1,25 @@
 import java.util.*;
 
 class Solution {
+
     public List<List<Integer>> subsets(int[] nums) {
-        // TODO: solve
-        return new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(
+        List<List<Integer>> list,
+        List<Integer> temp,
+        int[] nums,
+        int start
+    ) {
+        list.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(list, temp, nums, i + 1);
+            temp.remove(temp.size() - 1);
+        }
     }
 }
