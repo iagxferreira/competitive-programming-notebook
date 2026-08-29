2c0bb270264815176c71022cc6f24289e4ea6d7e
Iago Ferreira <iago-ferreira@outlook.com>
2026-08-29 03:28:01 -0300

    feat: solving cloned binary tree correpondent node

--- leetcode/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/Solution.java
+++ leetcode/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/Solution.java
@@ -4,8 +4,10 @@ class Solution {
     public final TreeNode getTargetCopy(final TreeNode original,
                                         final TreeNode cloned,
                                         final TreeNode target) {
-        // TODO: solve
-        return null;
+        if(original == null || original == target) return cloned;
+        var res = getTargetCopy(original.left, cloned.left, target);
+        if(res == null) return getTargetCopy(original.right, cloned.right, target);
+        return res;
     }
 }
 
