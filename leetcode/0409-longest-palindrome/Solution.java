13594411f17059a7deebaedc338f9539680909e0
Iago Ferreira <iago-ferreira@outlook.com>
2026-08-29 02:18:02 -0300

    feat: solving palindrome with hashset

--- leetcode/0409-longest-palindrome/Solution.java
+++ leetcode/0409-longest-palindrome/Solution.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 class Solution {
     public int longestPalindrome(String s) {
-        // TODO: solve
-        return 0;
+        Set<Character>set = new HashSet<>();
+        var n = 0;
+        for(int i = 0; i <s.length(); i++){
+            var ch = s.charAt(i);
+
+            if (set.contains(ch)){
+               n++;
+              set.remove(ch);
+            }else {
+                set.add(ch);
+            }
+        }
+        return n * 2 == s.length() ? n* 2 : n*2 + 1;
     }
 }
