2ab1f8e5536dab404182d152158300972b32e935
Iago Ferreira <iago-ferreira@outlook.com>
2026-08-29 01:55:14 -0300

    feat: pushing ransom-note solution

--- leetcode/0383-ransom-note/Solution.java
+++ leetcode/0383-ransom-note/Solution.java
@@ -2,7 +2,17 @@ import java.util.*;
 
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // TODO: solve
-        return false;
+        if(ransomNote.length() > magazine.length()) return false;
+        var counter = new int[128];
+
+        for(var letter :  magazine.toCharArray()){
+            ++counter[letter];
+        }
+
+        for(var letter :  ransomNote.toCharArray()){
+            if(--counter[letter]<0) return false;
+        }
+
+        return true;
     }
 }
