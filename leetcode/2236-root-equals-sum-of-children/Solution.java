e8e151a78041b263e1417443a6eba59aeee1bb66
Iago Ferreira <iago-ferreira@outlook.com>
2026-08-29 03:15:57 -0300

    feat: added easy problem solution

--- leetcode/2236-root-equals-sum-of-children/Solution.java
+++ leetcode/2236-root-equals-sum-of-children/Solution.java
@@ -2,8 +2,7 @@ import java.util.*;
 
 class Solution {
     public boolean checkTree(TreeNode root) {
-        // TODO: solve
-        return false;
+        return root.val == root.right.val + root.left.val;
     }
 }
 
