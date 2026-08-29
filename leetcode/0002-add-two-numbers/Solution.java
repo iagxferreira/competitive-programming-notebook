cc30968c03324dd25fd769b75985c2ac703a3a0e
Iago Ferreira <iago-ferreira@outlook.com>
2026-08-29 01:04:55 -0300

    feat: committing two numbers

--- leetcode/0002-add-two-numbers/Solution.java
+++ leetcode/0002-add-two-numbers/Solution.java
@@ -2,18 +2,29 @@ import java.util.*;
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        int carry = 0;
+        var dummy = new ListNode(0);
+        var tail = dummy;
+        var carry = 0;
 
         while(l1 != null|| l2 != null || carry != 0){
             int d1 = (l1 != null) ? l1.val : 0;
             int d2 = (l2 != null) ? l2.val : 0;
 
-            int sum = d1
+            int sum = d1 + d2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            var node = new ListNode(digit);
+            tail.next = node;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
         }
 
-        return null;
+        var result = dummy.next;
+        dummy.next = null;
+        return result;
     }
 }
 
